537c388745a138f17d26cf8c4576376cf93cd4da
Nikolas Garofil <nikolas@garofil.be>
2021-04-21 21:24:54 +0200

    nickserv

--- startshell.sh
+++ startshell.sh
@@ -16,4 +16,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+echo 'Start the bot in the container with: garobot.pl [--server=yourserver] [--nickserv]'
+echo 'Without "--server" the default one will be used (see /usr/local/readonlydata/settings)'
+echo 'With "--nickserv" you will have to enter a password which will be used to identify the bot'
 docker run --pids-limit 80 --memory 100m --cpus 0.2 --read-only --tmpfs /tmp:rw,exec,size=100m --tmpfs /home/user:rw,exec,size=100m -v $PWD/readonlydata:/usr/local/readonlydata:ro -v $PWD/code:/usr/local/sbin:ro -it --rm --name garobot garobot /bin/bash
